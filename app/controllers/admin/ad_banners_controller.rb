110ad9dc99901c48f9eb47be7a2ddd764d326432
jfqd <jfqd@blun.org>
2015-11-18 14:11:39 +0100

    restrict ads administration by a role

--- app/controllers/admin/ad_banners_controller.rb
+++ app/controllers/admin/ad_banners_controller.rb
@@ -1,3 +1,9 @@
 class Admin::AdBannersController < Admin::ResourceController
   model_class AdBanner
+  
+  only_allow_access_to :index, :new, :create, :edit, :update, :sort, :destroy, :delete,
+    :when => [:admin, :ads],
+    :denied_url => { :controller => 'pages', :action => 'index' },
+    :denied_message => 'You must have administrative privileges to perform this action.'
+  
 end
