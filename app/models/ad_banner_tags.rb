62add69e64645e7635671133f9bf04f40fb19009
jfqd <jfqd@blun.org>
2016-02-02 10:32:04 +0100

    do not escape url

--- app/models/ad_banner_tags.rb
+++ app/models/ad_banner_tags.rb
@@ -12,21 +12,17 @@ module AdBannerTags
   tag 'ad_banner' do |tag|
     @selected_banners ||= []
     ad_banner = if tag.attr['name']
-                  AdBanner.find_by_name(tag.attr['name'], :joins => "INNER JOIN assets ON assets.id = ad_banners.asset_id")
-                else
-                  AdBanner.select_banner(:exclude => @selected_banners, :format => tag.attr['format'])
-                end
+      AdBanner.find_by_name(tag.attr['name'], :joins => "INNER JOIN assets ON assets.id = ad_banners.asset_id")
+    else
+      AdBanner.select_banner(:exclude => @selected_banners, :format => tag.attr['format'])
+    end
     unless ad_banner.nil?
       @selected_banners << ad_banner.id
       # The HTML is simple enough to roll by hand instead of sucking in REXML
-      returning String.new do |result|
-        unless ad_banner.link_url.blank?
-          result << %Q{<a href="#{CGI.escapeHTML(ad_banner.link_url)}"}
-          result << %Q{ target="#{ad_banner.link_target}"}
-          result << '>'
-        end
+      String.new.tap |result|
+        result << %Q{<a href="#{ad_banner.link_url}" target="#{ad_banner.link_target}">} unless ad_banner.link_url.blank?
         result << %Q{<img src="#{ad_banner.asset.thumbnail(:original)}" title="#{ad_banner.name}" alt="#{ad_banner.asset.caption || ad_banner.asset.title}" />}
-        result << '</a>' unless ad_banner.link_url.blank?
+        result << %Q{</a>} unless ad_banner.link_url.blank?
       end
     end
   end
