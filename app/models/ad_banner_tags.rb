e440c041569871601acd330d2d9460ae4e4c87ef
jfqd <jfqd@blun.org>
2016-02-02 10:38:37 +0100

    add missing do

--- app/models/ad_banner_tags.rb
+++ app/models/ad_banner_tags.rb
@@ -19,12 +19,12 @@ module AdBannerTags
     unless ad_banner.nil?
       @selected_banners << ad_banner.id
       # The HTML is simple enough to roll by hand instead of sucking in REXML
-      String.new.tap |result|
+      String.new.tap do |result|
         result << %Q{<a href="#{ad_banner.link_url}" target="#{ad_banner.link_target}">} unless ad_banner.link_url.blank?
         result << %Q{<img src="#{ad_banner.asset.thumbnail(:original)}" title="#{ad_banner.name}" alt="#{ad_banner.asset.caption || ad_banner.asset.title}" />}
         result << %Q{</a>} unless ad_banner.link_url.blank?
-      end
-    end
-  end
+      end # String.new.tap
+    end # unless ad_banner.nil?
+  end # tag 'ad_banner'
 
 end
