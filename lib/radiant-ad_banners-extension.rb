bd684bed3583f867918fecbe1e74e4707a5162bd
jfqd <jfqd@blun.org>
2026-01-22 22:39:37 +0100

    bump version to 0.10.8

--- lib/radiant-ad_banners-extension.rb
+++ lib/radiant-ad_banners-extension.rb
@@ -1,5 +1,5 @@
 module RadiantAdBannersExtension
-  VERSION     = "0.10.7"
+  VERSION     = "0.10.8"
   SUMMARY     = "Manage ad banners"
   DESCRIPTION = "Manage ad banners"
   URL         = "https://github.com/davec/radiant-ad_banners-extension"
